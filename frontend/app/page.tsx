93cbb8afc569e004ec8316fa5f7c43e4d16b5f77
Benjamin Yang <benjamin.yang12@outlook.com>
2026-01-06 12:11:25 +0100

    sent leaderboard'

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,24 @@
+"use client";
+
 export default function Home() {
+  const getMurderer = async () => {
+    let output = "";
+
+    const res = await fetch('/api/users');
+    if (!res.ok) {
+      alert(`an error occured: ${res.status} ${res.statusText}`);
+    }
+
+    const data: {user: string, count: number}[] = await res.json();
+    data.sort((a, b) => b.count - a.count);
+
+    for (const a of data) {
+      output += `${a.user}: ${a.count} threats sent\n`;
+    }
+
+    alert(output);
+  }
+
   return (
     <div className="bg-zinc-1000 min-h-screen flex items-center justify-center">
       <div className="bg-zinc-900 p-6 rounded-md shadow-md">
@@ -10,6 +30,8 @@ export default function Home() {
           </div>
           <input type="submit" className="cursor-pointer bg-white px-4 py-1 rounded-md text-black hover:bg-gray-200 focus:outline-none focus:bg-gray-200" value="Log in"/>
         </form>
+        <br/>
+        <a onClick={() => getMurderer()} className="underline cursor-pointer text-xs text-gray-400">who's the murderer?</a>
       </div>
     </div>
   );
