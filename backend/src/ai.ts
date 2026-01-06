2c2be968e27c82f57161ed86aecd597055d4de47
david-why <david_why@outlook.com>
2026-01-06 17:17:32 +0100

    thing

--- backend/src/ai.ts
+++ backend/src/ai.ts
@@ -1,40 +1,46 @@
-import z, { toJSONSchema } from "zod";
+import z, { toJSONSchema } from 'zod';
 
 const { HACKCLUB_AI_KEY } = process.env;
 
-export const CheckDeathThreatSchema = z.object({
-  has_death_threat: z.boolean(),
-  reasoning: z.string(),
-});
+export const CheckDeathThreatSchema = z.union([
+  z.object({
+    has_death_threat: z.literal(false),
+    reasoning: z.string(),
+  }),
+  z.object({
+    has_death_threat: z.literal(true),
+    reasoning: z.literal(['weak', 'ok', 'strong']),
+  }),
+]);
 
 export async function checkDeathThreat(message: string) {
-  if (message.startsWith("[potato]")) {
-    return {"has_death_threat": true, "reasoning": "test message"}
+  if (message.startsWith('[potato]')) {
+    return { has_death_threat: true, reasoning: 'test message' };
   }
 
-  const res = await fetch("https://ai.hackclub.com/proxy/v1/chat/completions", {
-    method: "POST",
+  const res: any = await fetch('https://ai.hackclub.com/proxy/v1/chat/completions', {
+    method: 'POST',
     headers: {
       Authorization: `Bearer ${HACKCLUB_AI_KEY}`,
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      model: "qwen/qwen3-32b",
+      model: 'qwen/qwen3-32b',
       messages: [
         {
-          role: "system",
+          role: 'system',
           content:
             "You are an AI whose job is to check if a message contains a death threat. You should output a JSON object that identifies whether there is a death threat or not, and one sentence on why you think it is not if that is what you judge; else return 'weak', 'ok', or 'strong', based on how strong the threat is.",
         },
         {
-          role: "user",
+          role: 'user',
           content: message,
         },
       ],
       response_format: {
-        type: "json_schema",
+        type: 'json_schema',
         json_schema: {
-          name: "death_threat_detection",
+          name: 'death_threat_detection',
           strict: true,
           schema: toJSONSchema(CheckDeathThreatSchema),
         },
